40ff114346d06b06609b32db00d1057591dddaa7
Colby <vertiluscolby@gmail.com>
2023-05-10 19:48:13 -0300

    Backend Portafolio web

---
+++ JpaPersona/src/main/java/com/Person/demo/JpaPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.Person.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPersonaApplication.class, args);
+	}
+
+}
